f7bd478487e5a1499ff69514b61b17b0e1e501ee
Eugene <eugene.fedurkin@gmail.com>
2017-07-27 21:08:47 +0300

    add methods of store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,4 @@
-import { User, List } from '../models/';
+import { User, List, Item } from '../models/';
 
 import { Injectable } from '@angular/core';
 
@@ -24,10 +24,36 @@ export class Store {
   }
 
   public deleteList(currentList: List) {
-    for (let list of this.store.lists) {
-      if (list.title === currentList.title) {
-        // delete
+    for (let i = 0; i < this.store.lists.length; i++) {
+      if (this.store.lists[i].title === currentList.title) {
+        this.store.lists.splice(i, 1);
         return;
+      }
+    }
+  }
+  public addItem(currentList: List, currentItem: Item) {
+    for (let i = 0; i < this.store.lists.length; i++) {
+      if (currentList.title === this.store.lists[i].title) {
+        for (let item of this.store.lists[i].items) {
+          if (item.title === currentItem.title) {
+            console.log('item with this name already exists');
+            return;
+          }
+        }
+        this.store.lists[i].items.push(currentItem);
+      }
+    }
+  }
+
+  public deleteItem(currentList: List, currentItem: Item) {
+    for (let i = 0; i < this.store.lists.length; i++) {
+      if (currentList.title === this.store.lists[i].title) {
+        for (let j = 0; j < this.store.lists[i].items.length; j++) {
+          if (this.store.lists[i].items[j].title === currentItem.title) {
+            this.store.lists[i].items.splice(j, 1);
+            return;
+          }
+        }
       }
     }
   }
